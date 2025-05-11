00d54bb5b3045ac833ac243aed2c57cc8d95a470
Laura Castillo <lauracastillo16.lcg@gmail.com>
2025-05-11 11:49:19 +0200

    Creación de componentes

---
+++ src/components/ArticulesList.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Articule from '@/components/Articule.vue';
+const Articles= [{
+        title: 'Performat',
+        icon: '/src/images/USP-1.png',
+        description: 'Truly reactive, compiler-optimized rendering system that rarely requires manual optimization.',
+      },
+      {
+        title: 'Approchable',
+        icon: '/src/images/USP-2.png',
+        description: 'Builds on top of standard HTML, CSS and JavaScript with intuitive API and world-class documentation.',
+      },
+      {
+        title: 'Versatile',
+        icon: '/src/images/USP-3.png',
+        description: 'A rich, incrementally adoptable ecosystem that scales between a library and a full-featured framework.',
+      },
+
+      ]
+</script>
+
+<template>
+  <section >
+    <Articule
+      v-for="article in Articles"
+      :key="article.id"
+      :title="article.title"
+      :icon="article.icon"
+      :description="article.description"
+    ></Articule>
+  </section>
+</template>
+
+<style scoped>
+ section{
+    width: 100%;
+    height: auto;
+    background-color: #39B983;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 4rem;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav>
+    <img src="@/images/Ironhack_logo.svg" alt="ironHackLogo">
+    <img src="@/images/Burger_menu.svg" alt="menu">
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin:2rem 0rem 3rem 0rem; 
+}
+</style>
